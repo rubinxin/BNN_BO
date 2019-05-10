ae30c57e019fa103b85ac70ed072d9833a301095
Robin Ru <robin@robots.ox.ac.uk>
2019-05-11 00:48:31 +0100

    revert debug changes

--- models/lcbnn.py
+++ models/lcbnn.py
@@ -10,8 +10,7 @@ class LCBNNWarp(BaseModel):
                  n_units=[50, 50, 50],
                  dropout = 0.05, length_scale = 1e-2, T = 1000, util_type='se_y',
                  normalize_input=True, normalize_output=True, seed=42, actv='tanh'):
-
-        self.util_type = util_type
+        # self.model = \
         self.model = LCBNN(batch_size=mini_batch_size,num_epochs=num_epochs,
                  n_units_1=n_units[0], n_units_2=n_units[1], n_units_3=n_units[2],
                  dropout_p = dropout, length_scale = length_scale, T = T,
@@ -20,24 +19,18 @@ class LCBNNWarp(BaseModel):
 
     def _create_model(self, X, Y):
         Y = Y.flatten()
-        train_mes_loss, train_util = self.model.train(X, Y)
-        return train_mes_loss, train_util
+        self.model.train(X, Y)
 
-    def _update_model(self,  X_all, Y_all, file_name = None):
+    def _update_model(self,  X_all, Y_all):
         """
         Updates the model with new observations.
         """
         Y_all = Y_all.flatten()
 
         if self.model is None:
-            train_mes_loss, train_util = self._create_model(X_all, Y_all)
+            self._create_model(X_all, Y_all)
         else:
-            train_mes_loss, train_util = self.model.train(X_all, Y_all)
-
-        if file_name is not None:
-
-            np.save(f"{file_name}lcbnn_train_mes_loss_{self.util_type}", train_mes_loss)
-            np.save(f"{file_name}lcbnn_train_util_{self.util_type}", train_util)
+            self.model.train(X_all, Y_all)
 
     def predict(self, X):
         """
