23d60b5a1c5d2a40f6f35bde988061dbeb41eb95
Robin Ru <robin@robots.ox.ac.uk>
2019-05-28 12:30:11 +0100

    data for debug and new results

--- data/bash_script/LCCD2d.sh
+++
@@ -1,3 +0,0 @@
-python bo_general_exps.py -f='rosenbrock-2d' -m='LCCD' -acq='LCB' -bm='CL' -b=1 -nitr=60 -s=10 -uo='se_y'
-python bo_general_exps.py -f='branin-2d' -m='LCCD' -acq='LCB' -bm='CL' -b=1 -nitr=60 -s=10 -uo='se_y'
-python bo_general_exps.py -f='egg-2d' -m='LCCD' -acq='LCB' -bm='CL' -b=1 -nitr=60 -s=10 -uo='se_y'
--- data/bash_script/rosen.sh
+++
@@ -1,3 +0,0 @@
-python bo_general_exps.py -f='rosenbrock-2d' -m='LCBNN' -acq='LCB' -bm='CL' -b=1 -nitr=60 -s=10
-python bo_general_exps.py -f='rosenbrock-2d' -m='MCDROP' -acq='LCB' -bm='CL' -b=1 -nitr=60 -s=10
-python bo_general_exps.py -f='rosenbrock-2d' -m='MCCONC' -acq='LCB' -bm='CL' -b=1 -nitr=60 -s=10
